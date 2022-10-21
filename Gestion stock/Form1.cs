317af3f47634f672b9d99f391fd6afe12006a7e4
Quentin-Dare <quentin.dare@yahoo.fr>
2022-10-21 16:37:59 +0200

    Ajoutez des fichiers projet.

---
+++ Gestion stock/Form1.cs
@@ -0,0 +1,26 @@
+namespace Gestion_stock
+{
+    public partial class Vin : Form
+    {
+        public Vin()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTableauBord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Vin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Gestion stock/Forms/Tableau_de_bord.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Gestion_stock.Forms
+{
+    partial class Tableau_de_bord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Board = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.Board);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1910, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Board
+            // 
+            this.Board.AutoSize = true;
+            this.Board.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Board.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Board.Location = new System.Drawing.Point(311, 23);
+            this.Board.Name = "Board";
+            this.Board.Size = new System.Drawing.Size(286, 36);
+            this.Board.TabIndex = 0;
+            this.Board.Text = "Tableau de bord";
+            this.Board.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tableau_de_bord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 559);
+            this.Controls.Add(this.panel1);
+            this.Name = "Tableau_de_bord";
+            this.Text = "Tableau_de_bord";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label Board;
+        private ImageList imageList1;
+    }
+}
